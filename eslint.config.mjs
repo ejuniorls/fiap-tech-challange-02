0f92c5e701566fb56cd3a1a17e59b0e9530d942b
Edmir Lôbo <ejuniorls@outlook.com>
2025-05-26 20:53:09 -0300

    prettier

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -3,9 +3,17 @@ import globals from "globals";
 import markdown from "@eslint/markdown";
 import { defineConfig } from "eslint/config";
 
-
 export default defineConfig([
-  { files: ["**/*.{js,mjs,cjs}"], plugins: { js }, extends: ["js/recommended"] },
+  {
+    files: ["**/*.{js,mjs,cjs}"],
+    plugins: { js },
+    extends: ["js/recommended"],
+  },
   { files: ["**/*.{js,mjs,cjs}"], languageOptions: { globals: globals.node } },
-  { files: ["**/*.md"], plugins: { markdown }, language: "markdown/gfm", extends: ["markdown/recommended"] },
+  {
+    files: ["**/*.md"],
+    plugins: { markdown },
+    language: "markdown/gfm",
+    extends: ["markdown/recommended"],
+  },
 ]);
